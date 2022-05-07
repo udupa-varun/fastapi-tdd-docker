a11e17c5eb8a2b561ec52cb44136f3785b57b19a
Varun Udupa <udupa.varun@gmail.com>
2022-05-07 15:41:10 +0530

    feat(part1): added postgres, tortoise, pytest

--- project/app/main.py
+++ project/app/main.py
@@ -1,16 +1,32 @@
 # project/app/main.py
+import logging
 
 from fastapi import FastAPI, Depends
 
-from app.config import get_settings, Settings
+# from tortoise.contrib.fastapi import register_tortoise
+from app.api import ping
+from app.db import init_db
 
-app = FastAPI()
+log = logging.getLogger("uvicorn")
 
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing,
-    }
+def create_application() -> FastAPI:
+    application = FastAPI()
+
+    application.include_router(ping.router)
+
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
