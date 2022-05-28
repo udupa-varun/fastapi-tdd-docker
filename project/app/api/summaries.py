e23cc3333c675169b3b372670fe840ee77830448
Varun Udupa <udupa.varun@gmail.com>
2022-05-28 15:37:33 +0530

    feat(routes): added more routes to app (and tests)

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,9 +1,13 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
-from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
+from app.models.pydantic import (
+    SummaryPayloadSchema,
+    SummaryResponseSchema,
+    SummaryUpdatePayloadSchema,
+)
 from app.models.tortoise import SummarySchema
 
 router = APIRouter()
@@ -20,8 +24,8 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
     return response_object
 
 
-@router.get("/{id}", response_model=SummarySchema)
-async def read_summary(id: int) -> SummarySchema:
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int = Path(..., gt=0)) -> SummarySchema:
     summary = await crud.get(id)
     if not summary:
         raise HTTPException(status_code=404, detail="Summary not found")
@@ -32,3 +36,24 @@ async def read_summary(id: int) -> SummarySchema:
 @router.get("/", response_model=List[SummarySchema])
 async def read_all_summaries() -> List[SummarySchema]:
     return await crud.get_all()
+
+
+@router.delete("/{id}/", response_model=SummaryResponseSchema)
+async def delete_summary(id: int = Path(..., gt=0)) -> SummaryResponseSchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    await crud.delete(id)
+
+    return summary
+
+
+@router.put("/{id}/", response_model=SummarySchema)
+async def update_summary(
+    payload: SummaryUpdatePayloadSchema, id: int = Path(..., gt=0)
+) -> SummarySchema:
+    summary = await crud.put(id, payload)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+    return summary
